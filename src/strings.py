d7927b028f555cd92fbb684f69b6f624d0e4652b
ian-antking <ian.antking@gmail.com>
2019-03-19 14:48:52 +0000

    removed answers from strings src4

--- src/strings.py
+++ src/strings.py
@@ -1,32 +1,32 @@
 def say_hello(string):
   # concatenate the string to the word "Hello, " and add an exclamation on the end
   # say_hello('worlds') should return "Hello, world!"
-  return 'Hello, ' + string + '!'
+  return
 
 def concatenate(string1, string2):
   # concatenate the two strings together and return the result
-  return string1 + string2
+  return
 
 def upper_case(string):
   # convert all the letters in the string to upper case and return the result
-  return string.upper()
+  return
 
 def lower_case(string):
   # convert all the letters in the string to lower case and return the result
-  return string.lower()
+  return
 
 def string_length(string):
   # return the length (number of characters) in the string
-  return len(string)
+  return
 
 def first_character(string):
   # return the first character of the string
-  return string[0]
+  return
 
 def last_character(string):
   # return the last character of the string
-  return string[-1]
+  return
 
 def specific_character(string, index):
   # return the character of the string stored in the index variable
-  return string[index]
+  return
