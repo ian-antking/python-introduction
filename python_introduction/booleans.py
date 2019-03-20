c7f0d6d2a4729676df0c8d57ad8f64e79b39cd62
Ian King <ian.antking@gmail.com>
2019-03-20 23:03:07 +0000

    added booleans tests

--- python_introduction/booleans.py
+++ python_introduction/booleans.py
@@ -1,64 +1,66 @@
-def negate(bool):
-  # return the opposite of the boolean
-  return
+import math
 
-def truthiness(bool):
+def negate(bool1):
+  # return the opposite of the boolean
+  return not bool1
+
+def truthiness(bool1):
   # return the truthiness of the boolean
-  return
+  return bool(bool1)
 
 def both(bool1, bool2):
   #return true if both booleans are true
-  return
+  return bool1 and bool2
 
 def neither(bool1, bool2):
   # return true if neither of the booleans are true
-  return
+  return not bool1 and not bool2
 
 def either(bool1, bool2):
   # return true if either of the booleans are true
-  return
+  return bool1 or bool2
 
 def exactly_one(bool1, bool2):
   # return true if exatcly one of the booleans are true
   # not both
-  return
+  return (bool1 and not bool2) or (not bool1 and bool2)
 
 def is_equal(a, b):
   # return true if a and b are equal
-  return
+  return a == b
 
 def greater_than(a, b):
   # return true is a is greater than b
-  return
+  return a > b
 
 def less_than(a, b):
   # return true if a is less than b
-  return
+  return a < b
 
 def greater_than_or_equal(a, b):
   # return true if a is greater than or equal to b
-  return
+  return a >= b
 
 def less_than_or_equal(a, b):
   # return true if a is less than or equal to b
-  return
+  return a <= b
 
 def is_odd(num):
   # return true if the number is odd
-  return
+  return num % 2 != 0
 
 def is_even(num):
   # return true is the number is even
-  return
+  return num % 2 == 0
 
 def is_square(num):
   # return true if the number is a square number
-  return
+  return math.sqrt(num) % 1 == 0
 
 def starts_with(string, char):
   # return true if the first character of the string is the same as char
-  return
+  return string[0] == char
 
 def is_lower(string):
   # return true if all the letters in the string are lower case
-  return
+  return string.lower() == string
--- tests/booleans_tests.py
+++ tests/booleans_tests.py
@@ -2,5 +2,113 @@ import unittest
 from python_introduction import booleans
 
 class TestBooleans(unittest.TestCase):
-  def test_negate(self):
+  def test_negate_1(self):
     self.assertEqual(booleans.negate(True), False)
+
+  def test_negate_2(self):
+    self.assertEqual(booleans.negate(False), True)
+
+  def test_truthiness_1(self):
+    self.assertEqual(booleans.truthiness(''), False)
+  
+  def test_truthiness_2(self):
+    self.assertEqual(booleans.truthiness(0), False)
+
+  def test_truthiness_3(self):
+    self.assertEqual(booleans.truthiness([]), False)
+
+  def test_both_1(self):
+    self.assertEqual(booleans.both(True, True), True)
+
+  def test_both_2(self):
+    self.assertEqual(booleans.both(True, False), False)
+
+  def test_neither_1(self):
+    self.assertEqual(booleans.neither(True, False), False)
+
+  def test_neither_2(self):
+    self.assertEqual(booleans.neither(False, False), True)
+
+    def test_neither_3(self):
+      self.assertEqual(booleans.neither(True, True), False)
+
+  def test_either_1(self):
+    self.assertEqual(booleans.either(True, False), True)
+
+  def test_either_2(self):
+    self.assertEqual(booleans.either(False, False), False)
+
+  def test_exactly_one_1(self):
+    self.assertEqual(booleans.exactly_one(True, False), True)
+
+  def test_exactly_one_2(self):
+    self.assertEqual(booleans.exactly_one(True, True), False)
+
+  def test_is_equal_1(self):
+    self.assertEqual(booleans.is_equal(1, 1), True)
+
+  def test_is_equal_2(self):
+    self.assertEqual(booleans.is_equal(1, 'one'), False)
+
+  def test_greater_than_1(self):
+    self.assertEqual(booleans.greater_than(1, 3), False)
+  
+  def test_greater_than_2(self):
+    self.assertEqual(booleans.greater_than(3, 2), True)
+
+  def test_less_than_1(self):
+    self.assertEqual(booleans.less_than(1, 3), True)
+  
+  def test_less_than_2(self):
+    self.assertEqual(booleans.less_than(3, 2), False)
+
+  def test_greater_or_equal_1(self):
+    self.assertEqual(booleans.greater_than_or_equal(2, 2), True)
+  
+  def test_greater_or_equal_2(self):
+    self.assertEqual(booleans.greater_than_or_equal(2, 1), True)
+
+  def test_greater_or_equal_3(self):
+    self.assertEqual(booleans.greater_than_or_equal(1, 2), False)
+
+  def test_less_or_equal_1(self):
+    self.assertEqual(booleans.less_than_or_equal(2, 2), True)
+  
+  def test_less_or_equal_2(self):
+    self.assertEqual(booleans.less_than_or_equal(2, 1), False)
+
+  def test_less_or_equal_3(self):
+    self.assertEqual(booleans.less_than_or_equal(1, 2), True)
+
+  def test_is_odd_1(self):
+    self.assertEqual(booleans.is_odd(3), True)
+
+  def test_is_odd_2(self):
+    self.assertEqual(booleans.is_odd(2), False)
+
+  def test_is_even_1(self):
+    self.assertEqual(booleans.is_even(3), False)
+
+  def test_is_even_2(self):
+    self.assertEqual(booleans.is_even(2), True)
+
+  def test_is_square_1(self):
+    self.assertEqual(booleans.is_square(16), True)
+
+  def test_is_square_2(self):
+    self.assertEqual(booleans.is_square(10), False)
+
+  def test_starts_with_1(self):
+    self.assertEqual(booleans.starts_with('Hello', 'h'), False)
+
+  def test_starts_with_2(self):
+    self.assertEqual(booleans.starts_with('Hello', 'H'), True)
+
+  def test_starts_with_2(self):
+    self.assertEqual(booleans.starts_with('Hello', 3), False)
+
+  def test_is_lower_1(self):
+    self.assertEqual(booleans.is_lower('hello'), True)
+
+  def test_is_lower_2(self):
+    self.assertEqual(booleans.is_lower('Hello'), False)
