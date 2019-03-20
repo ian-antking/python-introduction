ff06859aaf2527058b05081b0a804e7d2df26a9a
Ian King <ian.antking@gmail.com>
2019-03-20 22:16:33 +0000

    made boolean functions

---
+++ python_introduction/booleans.py
@@ -0,0 +1,64 @@
+def negate(bool):
+  # return the opposite of the boolean
+  return
+
+def truthiness(bool):
+  # return the truthiness of the boolean
+  return
+
+def both(bool1, bool2):
+  #return true if both booleans are true
+  return
+
+def neither(bool1, bool2):
+  # return true if neither of the booleans are true
+  return
+
+def either(bool1, bool2):
+  # return true if either of the booleans are true
+  return
+
+def exactly_one(bool1, bool2):
+  # return true if exatcly one of the booleans are true
+  # not both
+  return
+
+def is_equal(a, b):
+  # return true if a and b are equal
+  return
+
+def greater_than(a, b):
+  # return true is a is greater than b
+  return
+
+def less_than(a, b):
+  # return true if a is less than b
+  return
+
+def greater_than_or_equal(a, b):
+  # return true if a is greater than or equal to b
+  return
+
+def less_than_or_equal(a, b):
+  # return true if a is less than or equal to b
+  return
+
+def is_odd(num):
+  # return true if the number is odd
+  return
+
+def is_even(num):
+  # return true is the number is even
+  return
+
+def is_square(num):
+  # return true if the number is a square number
+  return
+
+def starts_with(string, char):
+  # return true if the first character of the string is the same as char
+  return
+
+def is_lower(string):
+  # return true if all the letters in the string are lower case
+  return
